321a2970ecb84c9a387e7224a2a09b9c166e778e
ldwv6 <ldwv6@naver.com>
2020-06-10 09:39:19 +0900

    Add JWT 미들웨어

---
+++ Password-Reset-JWT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Password_Reset_JWT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
