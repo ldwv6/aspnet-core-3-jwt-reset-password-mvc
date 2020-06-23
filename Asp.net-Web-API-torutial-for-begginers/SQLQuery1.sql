6c6cb880c3971553724a8c3adeab2ea19a4e5c37
ldwv6 <ldwv6@naver.com>
2020-06-23 14:04:38 +0900

    Add 22 torurial

--- Asp.net-Web-API-torutial-for-begginers/SQLQuery1.sql
+++ Asp.net-Web-API-torutial-for-begginers/SQLQuery1.sql
@@ -1,18 +1,16 @@
-﻿
-
-
-Create table Employees
+﻿CREATE TABLE EMployees 
 (
 	ID int primary key identity,
-	FirstName nvarchar(50),
-	LastName nvarchar(50),
-	Gender nvarchar(50),
-	Salary int
+	FirstName NVARCHAR(50),
+	LastName NVARCHAR(50),
+	Gender NVARCHAR(50),
+	Salary int 
 )
 
-Go
 
-Insert into Employees values ('Mark', 'Hastings', 'Male',60000)
-
-
-
+insert into EMployees Values('Mark','Hastings','Male',60000)
+insert into EMployees Values('Steve','Pound','Male',45000)
+insert into EMployees Values('Ben','Hoskins','Male',70000)
+insert into EMployees Values('Phlip','Hastings','Male',45000)
+insert into EMployees Values('Mary','Lambeth','Female',30000)
+insert into EMployees Values('Valarie','Vikings','Female',35000)
---
+++ EmpoyeeService/Controllers/EmployeeServiceController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmpoyeeService.Controllers
+{
+    public class EmployeeServiceController : ApiController
+    {
+        [Authorize]
+        public IEnumerable<EMployees> Get()
+        {
+            using(employeeDBEntities entites = new employeeDBEntities())
+            {
+                return entites.EMployees.ToList();
+            }
+        }
+    }
+}
